d9d7e70b7218668e8dcfe2e4013034ff8b8ce56c
Frost-704 <boolean.pg.me@gmail.com>
2023-10-29 15:38:43 +0300

    finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.min = 0;
+      this.max = 0;
+      this.target = 0;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
+      this.target = Math.ceil(Math.random() * (max - min)) + min;
     }
-
+  
     guess() {
-
+      return Math.ceil((this.min + this.max) / 2);
     }
-
+  
     lower() {
-
+      this.max = this.guess();
     }
-
+  
     greater() {
-
+      this.min = this.guess();
     }
-}
+  }
+  
+  
+  
+  module.exports = GuessingGame;
 
 module.exports = GuessingGame;
